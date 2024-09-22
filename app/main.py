55326c7f37af6fa4a372aba6da5478d5abca5fda
raunak-sapariya <raunaksapariya@gmail.com>
2024-09-23 02:30:02 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -175,21 +175,22 @@ def tokenize(file_contents):
                         lexical_errors = True
                         pointer += 1
                 
-                case "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9": 
+                case "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"|".": 
                     start = pointer
-                    while pointer + 1 < len(file_contents) and (file_contents[pointer + 1] in "0123456789"):
+                    if pointer + 1 < len(file_contents) and (file_contents[pointer + 1] in "0123456789."):
+                        while pointer + 1 < len(file_contents) and (file_contents[pointer + 1] in "0123456789"):
+                            pointer += 1
+                        if pointer + 1 < len(file_contents) and file_contents[pointer + 1] == ".":
+                            pointer += 1
+                            if pointer + 1 < len(file_contents) and (file_contents[pointer + 1] in "0123456789"):
+                                while pointer + 1 < len(file_contents) and file_contents[pointer + 1] in "0123456789":
+                                    pointer += 1
+                        value = file_contents[start:pointer + 1]
+                        tokens.append(f"NUMBER {value} {float(value)}")
                         pointer += 1
-                    if pointer + 1 < len(file_contents) and file_contents[pointer + 1] == ".":
+                    else:
+                        tokens.append("DOT . null")
                         pointer += 1
-                        if pointer + 1 < len(file_contents) and (file_contents[pointer + 1] in "0123456789"):
-                            while pointer + 1 < len(file_contents) and file_contents[pointer + 1] in "0123456789":
-                                pointer += 1
-                    value = file_contents[start:pointer + 1]
-                    tokens.append(f"NUMBER {value} {float(value)}")
-                    pointer += 1
-                case ".": 
-                    tokens.append("DOT . null")
-                    pointer += 1
                 case "a"|"b"|"c"|"d"|"e"|"f"|"g"|"h"|"i"|"j"|"k"|"l"|"m"|"n"|"o"|"p"|"q"|"r"|"s"|"t"|"u"|"v"|"w"|"x"|"y"|"z"|"A"|"B"|"C"|"D"|"E"|"F"|"G"|"H"|"I"|"J"|"K"|"L"|"M"|"N"|"O"|"P"|"Q"|"R"|"S"|"T"|"U"|"V"|"W"|"X"|"Y"|"Z"|"_"|"0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9":
                     start = pointer
                     while pointer + 1 < len(file_contents) and file_contents[pointer + 1] in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789":
