30af4957e0953aef45ebaf19d0f3e696a485a4a2
raunak-sapariya <raunaksapariya@gmail.com>
2024-09-23 11:02:09 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -131,45 +131,45 @@ def tokenize(file_contents):
                         tokens.append(error_message)
                         lexical_errors = True
                         pointer += 1
-                case "'":
-                    start = pointer
-                    if pointer + 2 < len(file_contents) and (file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'" and file_contents[pointer - 1] == "="):
-                        pointer += 3
-                        while pointer + 1 < len(file_contents) and not(file_contents[pointer] == "'" and file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'"):
-                            pointer += 1
-                        if pointer + 1 < len(file_contents) and (file_contents[pointer] == "'" and file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'"):
-                            value = file_contents[start + 3:pointer]
-                            pointer += 3
-                            tokens.append(f"STRING '{value}' {value}")
-                        else:
-                            tokens.append(f"[line {line_number}] Error: Unterminated MULTI-LINE STRING ''' '''.")
-                            lexical_errors = True
-                            pointer += 1
-                    elif(pointer + 1 < len(file_contents) and file_contents[pointer - 1] == "="):
-                        while pointer + 1 < len(file_contents) and file_contents[pointer + 1] != "'":
-                            pointer += 1
-                        if pointer + 1 < len(file_contents) and file_contents[pointer + 1] == "'":
-                            value = file_contents[start + 1:pointer + 1]
-                            tokens.append(f"STRING '{value}' {value}")
-                            pointer += 2
-                        else:
-                            tokens.append(f"[line {line_number}] Error: Unterminated string.")
-                            lexical_errors = True
-                            pointer += 1
-                    elif(file_contents[pointer - 1] != "="):
-                        if pointer + 2 < len(file_contents) and (file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'" and file_contents[pointer - 1] != "="):
-                            pointer += 3
-                            while pointer < len(file_contents) and not(file_contents[pointer] == "'" and file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'"):
-                                pointer += 1
-                            pointer += 3
-                        else:
-                            tokens.append(f"[line {line_number}] Error: Multi-Line Comment.[''' ''']")
-                            lexical_errors = True
-                            pointer += 1
-                    else:
-                        tokens.append(f"[line {line_number}] Error: Invalid use of single quote.")
-                        lexical_errors = True
-                        pointer += 1
+                # case "'":
+                #     start = pointer
+                #     if pointer + 2 < len(file_contents) and (file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'" and file_contents[pointer - 1] == "="):
+                #         pointer += 3
+                #         while pointer + 1 < len(file_contents) and not(file_contents[pointer] == "'" and file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'"):
+                #             pointer += 1
+                #         if pointer + 1 < len(file_contents) and (file_contents[pointer] == "'" and file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'"):
+                #             value = file_contents[start + 3:pointer]
+                #             pointer += 3
+                #             tokens.append(f"STRING '{value}' {value}")
+                #         else:
+                #             tokens.append(f"[line {line_number}] Error: Unterminated MULTI-LINE STRING ''' '''.")
+                #             lexical_errors = True
+                #             pointer += 1
+                #     elif(pointer + 1 < len(file_contents) and file_contents[pointer - 1] == "="):
+                #         while pointer + 1 < len(file_contents) and file_contents[pointer + 1] != "'":
+                #             pointer += 1
+                #         if pointer + 1 < len(file_contents) and file_contents[pointer + 1] == "'":
+                #             value = file_contents[start + 1:pointer + 1]
+                #             tokens.append(f"STRING '{value}' {value}")
+                #             pointer += 2
+                #         else:
+                #             tokens.append(f"[line {line_number}] Error: Unterminated string.")
+                #             lexical_errors = True
+                #             pointer += 1
+                #     elif(file_contents[pointer - 1] != "="):
+                #         if pointer + 2 < len(file_contents) and (file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'" and file_contents[pointer - 1] != "="):
+                #             pointer += 3
+                #             while pointer < len(file_contents) and not(file_contents[pointer] == "'" and file_contents[pointer + 1] == "'" and file_contents[pointer + 2] == "'"):
+                #                 pointer += 1
+                #             pointer += 3
+                #         else:
+                #             tokens.append(f"[line {line_number}] Error: Multi-Line Comment.[''' ''']")
+                #             lexical_errors = True
+                #             pointer += 1
+                #     else:
+                #         tokens.append(f"[line {line_number}] Error: Invalid use of single quote.")
+                #         lexical_errors = True
+                #         pointer += 1
                 case "0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"|".": 
                     start = pointer
                     if pointer+1< len(file_contents) and ((file_contents[pointer] in "0123456789")):
