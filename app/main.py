6173be7ac14c997b7528186ffe4286fab4e1a4d9
raunak-sapariya <raunaksapariya@gmail.com>
2024-10-26 23:16:07 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -33,10 +33,14 @@ def main():
     elif command == "parse":
         tokens, lexical_errors = tokenize(file_contents)
         parse_result, parser_errors = parse(tokens)
-        if parser_errors:
-            exit(65)  
         for result in parse_result:
             print(result)       
+        if parser_errors:
+            exit(65)
+        else:
+            exit(0)
+
+   
     else:
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
