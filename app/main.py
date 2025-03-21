47e59e4461a6b36718d9641183808f09abe72702
Raunak Sapariya <77540095+raunak-sapariya@users.noreply.github.com>
2025-03-22 00:17:51 +0530

    Runtime Errors: Binary Operators (1/2) 1

--- app/main.py
+++ app/main.py
@@ -417,10 +417,8 @@ def evaluate(parse_result):
                     checkNumberOperands(expr[1], left, right)
                     result = "true" if left >= right else "false"
                 elif expr[1] == "==":
-                    checkNumberOperands(expr[1], left, right)
                     result = "true" if left == right else "false"
                 elif expr[1] == "!=":
-                    checkNumberOperands(expr[1], left, right)
                     result = "true" if left != right else "false"
                 else:
                     print(f"Unknown binary operator: {expr[1]}", file=sys.stderr)
